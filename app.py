c921a6e381aa9add3286e03a973d7437c3e023f4
biancab97 <biancabreeman@gmail.com>
2023-10-30 14:49:54 +0100

    app py aangepast

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import haven
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,16 +8,5 @@ def hello_world():
 
 @app.route("/Bianca")
 def Bianca():
-    var1 = 26
-    print(var1)
-    var1 += 100
-    return "<p>Hello, Bianca!</p>" +str(var1)
+    return "<p>Hello, Bianca!</p>"
 
-
-@app.route("/verzinnen/<eenvariabele>")
-def ietsverzinnen(eenvariabele):
-    return "<p>Hello, World!" +eenvariabele + " </p>" 
-
-@app.route("/haven")
-def havenmethode():
-    return haven.demethodevandehaven()
