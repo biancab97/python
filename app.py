66001f0b48fb29fee413607501c1b0a2ee495deb
biancab97 <biancabreeman@gmail.com>
2023-11-15 16:18:13 +0100

    echte versie

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import dash
 from dash import Dash, html, dcc, callback, Output, Input
 
 
@@ -22,31 +23,5 @@ dash_app.layout = html.Div(
     ]
 )
 
-if __name__ == "__main__":
-    dash_app.run_server(debug=True)
-
-from dash import Dash, html, dcc, callback, Output, Input
-
-#Create app and add the pages
-dash_app = dash.Dash(__name__, use_pages=True)
-
-#Create app for Azure deployment
-app = dash_app.server
-
-#Set layout of the entire app
-dash_app.layout = html.Div(
-    [#Framework of the main app
-        html.Div("Python MultiPage Match App", style={'fontSize': 50, 'textAlign':'center'}),
-        html.Div([
-            dcc.Link(page['name'] + " | ", href=page['path'])
-            for page in dash.page_registry.values()
-        ]),
-        html.Hr(),
-
-        #Content of each page
-        dash.page_container
-    ]
-)
-        
 if __name__ == "__main__":
     dash_app.run_server(debug=True)
--- pages/vacatures_regio.py
+++ pages/vacatures_regio.py
@@ -18,13 +18,13 @@ layout = html.Div([
     
     dcc.Dropdown(),
     #dash_table.DataTable(data=vacatures_regio_df.to_dict('records'), page_size=10),
-    dcc.Graph(figure={}, id='controls-and-graph')
+    dcc.Graph(figure={}, id='controls-and-graph2')
 ])
 
 #Add controls to build the interaction
 @callback(
-        Output(component_id='controls-and-graph', component_property='figure'),
-        Input(component_id='controls-and-radio-item', component_property='value')
+        Output(component_id='controls-and-graph2', component_property='figure'),
+        Input(component_id='controls-and-radio-item2', component_property='value')
 )
 def update_graph(col_chosen):
     fig = px.choropleth(
